b80a1aa6ce6d1d531a9a01e36a9d80dd2e7adcb7
Bastien98 <bastien.moenaert@gmail.com>
2024-09-29 18:57:04 +0200

    add adapter, method not implemented

---
+++ file-api/src/file_api/adapters/storage/local_vector_db_storge.py
@@ -0,0 +1,8 @@
+from langchain_core.documents import Document
+from file_api.core.domain.embeddings_model import Embeddings
+from file_api.core.ports.vector_db_port import VectorDbPort
+
+
+class LocalVectorDbAdapter(VectorDbPort):
+    async def save_embeddings(self, embeddings: Embeddings, vector_db_index: str) -> None:
+        pass
--- file-api/src/file_api/core/ports/vector_db_port.py
+++ file-api/src/file_api/core/ports/vector_db_port.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 from file_api.core.domain.embeddings_model import Embeddings
 
 
-class EmbeddingsPort(ABC):
+class VectorDbPort(ABC):
     @abstractmethod
     async def save_embeddings(self, embeddings: Embeddings, vector_db_index: str) -> None:
         pass
