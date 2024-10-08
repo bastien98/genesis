1118d77a2da6f41f9face56b576e53e9a60efea7
Bastien98 <bastien.moenaert@gmail.com>
2024-10-08 17:29:34 +0200

    add validation method but used

--- file-api-v2/src/file_api_v2/domain/entities/knowledge_base.py
+++ file-api-v2/src/file_api_v2/domain/entities/knowledge_base.py
@@ -11,3 +11,9 @@ class KnowledgeBase:
     def add_document(self, document: Document) -> None:
         self.docs.append(document)
         print(f"Document '{document.doc_name}' added to raw_docs in Knowledge Base '{self.kb_name}'")
+
+    def validate_document(self, document: Document) -> None:
+        # Check if a document with the same name already exists
+        if any(doc.doc_name == document.doc_name for doc in self.docs):
+            raise ValueError(
+                f"Document with name '{document.doc_name}' already exists in Knowledge Base '{self.kb_name}'")
