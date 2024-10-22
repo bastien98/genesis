70b4074a8b8977570bd44d3c8e78f11cd56e6190
Bastien98 <bastien.moenaert@gmail.com>
2024-10-22 21:53:15 +0200

    deleted old stuffs

--- api/src/config.py
+++ api/src/config.py
@@ -1,5 +1,2 @@
 APP_NAME = "super-rag"
 APP_VERSION = "1.0.0"
-UNIQUE_PAGE_DELIMMETER = "\nUNIQUE_PAGE_DELIMITER_%^!@#$%%^\n"
-PROCESSED_FILE_LOCATION = "../../data/processed"
-DB_CONNECTION_STR = "mysql+mysqlconnector://root:Gilles1998@localhost/superRag"
