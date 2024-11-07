7281a89d5562af93013e8703f97afbcc8aff3ba7
Bastien98 <bastien.moenaert@gmail.com>
2024-11-07 12:47:48 +0100

    renaming

--- api/src/services/file_storage_service.py
+++ api/src/services/file_storage_service.py
@@ -8,22 +8,22 @@ from ports.file_storage_port import FileStoragePort
 
 class FileStorageService:
     def __init__(self, storage_adapter: FileStoragePort):
-        self.store = storage_adapter
+        self.storage_adapter = storage_adapter
 
     def save_raw_document(self, raw_doc: RawDocument, location: str) -> None:
-        self.store.save_raw_file(raw_doc.content, location)
+        self.storage_adapter.save_raw_file(raw_doc.content, location)
 
     def save_text_chunks(self, text_chunks: List[str], location: str) -> None:
-        self.store.save_text_chunks(text_chunks, location)
+        self.storage_adapter.save_text_chunks(text_chunks, location)
 
     def read_text_chunks(self, location: str) -> list[str]:
-        return self.store.read_text_chunks(location)
+        return self.storage_adapter.read_text_chunks(location)
 
     def save_md_chunks(self, text_chunks: List[str], location: str) -> None:
-        self.store.save_md_chunks(text_chunks, location)
+        self.storage_adapter.save_md_chunks(text_chunks, location)
 
     def save_BM25_index(self, bm25_index: BM25Okapi, location: str) -> None:
-        self.store.save_BM25_index(bm25_index, location)
+        self.storage_adapter.save_BM25_index(bm25_index, location)
 
     def read_BM25_index(self, location: str) -> BM25Okapi:
-        return self.store.read_BM25_index(location)
+        return self.storage_adapter.read_BM25_index(location)
